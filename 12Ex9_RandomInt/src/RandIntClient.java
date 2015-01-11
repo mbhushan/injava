38fa7b6575d999ba0ac58c3854ec782251a9b5f9
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-11 13:41:06 +0530

    Generate random numbers between min, max

---
+++ 12Ex9_RandomInt/src/RandIntClient.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class RandIntClient {
+	public static int MIN = 0;
+	public static int MAX = 0;
+	public static int COUNT = 10;
+	public static void main(String [] args) {
+		
+		readInput();
+		System.out.println("Random numbers: ");
+		for (int i=0; i < COUNT; i++) {
+			int rVal = RandInt.randInt(MIN, MAX);
+			System.out.print(rVal + " ");
+		}
+		System.out.println();
+		System.out.println("DONE!");
+	}
+	
+	public static void readInput() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String str = br.readLine();
+			String [] strArr = str.split(" ");
+			MIN = Integer.parseInt(strArr[0]);
+			MAX = Integer.parseInt(strArr[1]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}

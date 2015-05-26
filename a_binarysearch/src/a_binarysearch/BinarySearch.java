1de3783cf8e5b6349fd6734b5c49d529b55cea48
manibhushan <manibhushan.cs@gmail.com>
2015-05-26 10:00:48 +0530

    added input method

--- a_binarysearch/src/a_binarysearch/BinarySearch.java
+++ a_binarysearch/src/a_binarysearch/BinarySearch.java
@@ -1,12 +1,40 @@
 package a_binarysearch;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BinarySearch {
 
+	private static int [] A;
 	public static void main(String [] args) {
-		
+		readInput();
+		printArray(A);
+	}
+	
+	public static void printArray(int [] data) {
+		if (data == null) {
+			return ;
+		}
+		for (int i=0; i<data.length; i++) {
+			System.out.print(data[i] + " ");
+		}
+		System.out.println();
 	}
 	
 	public static void readInput() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		try {
+			String [] str = br.readLine().split(" ");
+			int len = str.length;
+			A = new int[len];
+			for (int i=0; i<len; i++) {
+				A[i] = Integer.parseInt(str[i]);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }

b66d8857b337e0a58176b53b65e54921730aaac7
manibhushan <manibhushan.cs@gmail.com>
2015-06-08 06:42:24 +0530

    factorial of a number -recursive

---
+++ a_Factorial/src/Factorial.java
@@ -0,0 +1,39 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Factorial {
+
+	private static int N;
+	
+	public static void main(String [] args) {
+		System.out.print("Enter integer n: ");
+		readInput();
+//		System.out.println();
+		long fact = factorialRecursive(N);
+		System.out.println(N + "! is: " + fact);
+		
+	}
+	
+	public static long factorialRecursive(int n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		return n * factorialRecursive(n-1);
+	}
+	
+	public static void readInput() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String str = br.readLine().trim();
+			N = Integer.parseInt(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+/**
+ Enter integer n: 5
+5! is: 120
+*/

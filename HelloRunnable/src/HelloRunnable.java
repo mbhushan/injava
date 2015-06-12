693172d71003318f89d496cfaedd483f458a7cf7
Mani Bhushan <manibhushan.cs@gmail.com>
2015-06-12 23:02:48 +0530

    Update HelloRunnable.java

--- HelloRunnable/src/HelloRunnable.java
+++ HelloRunnable/src/HelloRunnable.java
@@ -2,7 +2,7 @@
 public class HelloRunnable implements Runnable {
 	
 	public void run() {
-		System.out.println("Hello Mani from a thread!");
+		System.out.println("Hello from a thread!");
 	}
 	
 	public static void main(String [] args) {

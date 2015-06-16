4dd883c2af763c03f084648e8dbe73cf3499f96e
manibhushan <manibhushan.cs@gmail.com>
2015-06-16 08:26:31 +0530

    find all three numbers in an integer array which sum to zero

--- a_ThreeSum/src/a_ThreeSum/ThreeSum.java
+++ a_ThreeSum/src/a_ThreeSum/ThreeSum.java
@@ -8,12 +8,25 @@ public class ThreeSum {
 	private static int [] A;
 	private static int N;
 	public static void main(String [] args) {
-		
+		readInput();
+		System.out.println("Elements in the array: ");
+		printArray(A);
 	}
 	
 	//print all the triplets such that A[i] + A[j] + A[k] = 0
 	public static void threeSumZero(int [] arr) {
 		
+	
+	}
+	
+	public static void printArray(int [] arr) {
+		if (null == arr) {
+			return;
+		}
+		for (int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 	}
 	
 	public static void readInput() {

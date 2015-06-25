79b606b25b06b2104d2aff9a89e122f319b2b74f
manibhushan <manibhushan.cs@gmail.com>
2015-06-25 09:00:02 +0530

    test methods for positional node removal

--- a_LinkedListOperations/src/a_LinkedListOperations/SLLClient.java
+++ a_LinkedListOperations/src/a_LinkedListOperations/SLLClient.java
@@ -17,6 +17,7 @@ public class SLLClient {
 			list.printList();
 		}
 		
+		System.out.println();
 		System.out.println("Testing remove operations.");
 		for (int i=0; i<2; i++) {
 			System.out.println("removing front ..");
@@ -25,7 +26,53 @@ public class SLLClient {
 			System.out.println("removing back ..");
 			System.out.println("removed: " + list.removeBack());
 			list.printList();
+			int pos = rand.nextInt(list.size());
+			System.out.println("removing position: " + pos);
+			System.out.println("removed: " + list.removePosition(pos));
+			list.printList();
 		}
 		
 	}
  }
+/**
+front insert23
+23 -> null
+front insert83
+83 -> 23 -> null
+front insert40
+40 -> 83 -> 23 -> null
+front insert42
+42 -> 40 -> 83 -> 23 -> null
+front insert69
+69 -> 42 -> 40 -> 83 -> 23 -> null
+front insert45
+45 -> 69 -> 42 -> 40 -> 83 -> 23 -> null
+front insert72
+72 -> 45 -> 69 -> 42 -> 40 -> 83 -> 23 -> null
+front insert94
+94 -> 72 -> 45 -> 69 -> 42 -> 40 -> 83 -> 23 -> null
+front insert14
+14 -> 94 -> 72 -> 45 -> 69 -> 42 -> 40 -> 83 -> 23 -> null
+front insert62
+62 -> 14 -> 94 -> 72 -> 45 -> 69 -> 42 -> 40 -> 83 -> 23 -> null
+
+Testing remove operations.
+removing front ..
+removed: 62
+14 -> 94 -> 72 -> 45 -> 69 -> 42 -> 40 -> 83 -> 23 -> null
+removing back ..
+removed: 23
+14 -> 94 -> 72 -> 45 -> 69 -> 42 -> 40 -> 83 -> null
+removing position: 3
+removed: 45
+14 -> 94 -> 72 -> 69 -> 42 -> 40 -> 83 -> null
+removing front ..
+removed: 14
+94 -> 72 -> 69 -> 42 -> 40 -> 83 -> null
+removing back ..
+removed: 83
+94 -> 72 -> 69 -> 42 -> 40 -> null
+removing position: 2
+removed: 69
+94 -> 72 -> 42 -> 40 -> null
+*/

5b506e14217da6f27aa39fb7b73f8ba258b28759
manibhushan <manibhushan.cs@gmail.com>
2015-10-28 18:56:43 +0530

    randomized quick sort client class

---
+++ a_RandomQuickSort/src/a_RandomQuickSort/RandQuickSortClient.java
@@ -0,0 +1,41 @@
+package a_RandomQuickSort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RandQuickSortClient {
+	
+	private static Integer [] A;
+
+	public static void main(String [] args) {
+		readInput();
+		RandomizedQuickSort.sort(A);
+		System.out.println("After Randomized Quick Sorting: ");
+		showData();
+	}
+	
+	public static void readInput() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String [] data = br.readLine().trim().split(" ");
+			int len = data.length;
+			A = new Integer[len];
+			for (int i=0; i<len; i++) {
+				A[i] = Integer.parseInt(data[i]);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void showData() {
+		if (null == A) {
+			return;
+		}
+		for (int i=0; i<A.length; i++) {
+			System.out.print(A[i] + " ");
+		}
+		System.out.println();
+	}
+}

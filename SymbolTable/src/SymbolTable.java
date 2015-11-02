3da80ebd08ab9380ec8334e1ccfb47e352dbb5c2
manibhushan <manibhushan.cs@gmail.com>
2015-11-02 14:41:42 +0530

    Symbol table generic APIs

---
+++ SymbolTable/src/SymbolTable.java
@@ -0,0 +1,29 @@
+
+public class SymbolTable<Key, Value> {
+	public SymbolTable() {}
+	
+	public Value get(Key key) {
+		return null;
+	}
+	
+	public void put(Key key, Value value) {}
+	
+	public void delete(Key key) {}
+	
+	public boolean contains(Key key) {
+		return false;
+	}
+	
+	public boolean isEmpty() {
+		return true;
+	}
+	
+	public int size() {
+		return -1;
+	}
+	
+	public Iterable<Key> keys() {
+		return null;
+	}
+
+}

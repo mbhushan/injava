2bc61343f0068db6c4f4a9a21825ea57c2cbe4e0
manibhushan <manibhushan.cs@gmail.com>
2015-11-16 17:01:56 +0530

    Sequential Search Symbol Table Implementation

---
+++ SequentialSearch/src/SequentialSearchST.java
@@ -0,0 +1,38 @@
+
+public class SequentialSearchST<Key, Value> {
+
+	private class Node {
+		Key key;
+		Value value;
+		Node next;
+		
+		public Node(Key key, Value value, Node next) {
+			this.key = key;
+			this.value = value;
+			this.next = next;
+		}
+	}
+	
+	//first node of the linkedlist
+	private Node first;
+	
+	public Value get(Key key) {
+		for (Node node = first; node != null; node = node.next) {
+			if (key.equals(node.key)) {
+				return node.value;
+			}
+		}
+		return null;
+	}
+	
+	public void put(Key key, Value value) {
+		for (Node node=first; node != null; node = node.next) {
+			if (key.equals(node.key)) {
+				node.value = value;
+				return;
+			}
+		}
+		// search miss, add this node to the front
+		first = new Node(key, value, first);
+	}
+}

1ccf9cab6c7dc636a4829c49a01c6ecd87772848
manibhushan <manibhushan.cs@gmail.com>
2015-12-22 21:59:20 +0530

    print diagonal

---
+++ i_Diagonal/src/i_Diagonal/Solution.java
@@ -0,0 +1,11 @@
+package i_Diagonal;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public ArrayList<ArrayList<Integer>> diagonal(ArrayList<ArrayList<Integer>> a) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		
+		return result;
+	}
+}

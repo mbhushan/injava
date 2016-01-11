0cd9385d94d9bfc7eecf6b180c1409130043cc7f
manibhushan <manibhushan.cs@gmail.com>
2016-01-11 14:18:24 +0530

    max gap in an array

--- i_MaxGap/src/i_MaxGap/MaxGap.java
+++ i_MaxGap/src/i_MaxGap/MaxGap.java
@@ -1,5 +1,20 @@
 package i_MaxGap;
 
+import java.util.List;
+
 public class MaxGap {
+	
+	public int maximumGap(final List<Integer> a) {
+		int maxgap = -1;
+		int n = a.size();
+		int start=0, end=n-1;
+		while (start < end) {
+			if (a.get(start) <= a.get(end)) {
+				maxgap = end - start;
+				break;
+			}
+		}
+		return maxgap;
+	}
 
 }

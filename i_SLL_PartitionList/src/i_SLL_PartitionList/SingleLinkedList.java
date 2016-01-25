e493d78b0a178899c7622fda1a79464abcfc4596
manibhushan <manibhushan.cs@gmail.com>
2016-01-25 07:19:46 +0530

    linked list class

---
+++ i_SLL_PartitionList/src/i_SLL_PartitionList/SingleLinkedList.java
@@ -0,0 +1,41 @@
+package i_SLL_PartitionList;
+
+class ListNode {
+	Integer val;
+	ListNode next;
+
+	public ListNode(Integer data) {
+		this.val = data;
+		next = null;
+	}
+}
+
+public class SingleLinkedList {
+
+	private ListNode head;
+	private int N;
+
+	public ListNode getHeadNode() {
+		return head;
+	}
+
+	public void insertFront(Integer data) {
+		ListNode node = new ListNode(data);
+		node.next = head;
+		head = node;
+		++N;
+	}
+
+	public int listSize() {
+		return N;
+	}
+
+	public void printList() {
+		ListNode curr = head;
+		while (curr != null) {
+			System.out.print(curr.val + " -> ");
+			curr = curr.next;
+		}
+		System.out.println("null");
+	}
+}

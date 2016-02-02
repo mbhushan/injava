ed9e2226fb3747e6c8d76ca0e37fc3422a2b5c09
manibhushan <manibhushan.cs@gmail.com>
2016-02-02 14:24:13 +0530

    spiral level order binary tree

--- i_BT_BinaryTree/src/i_BT_BinaryTree/BinaryTreeClient.java
+++ i_BT_BinaryTree/src/i_BT_BinaryTree/BinaryTreeClient.java
@@ -5,8 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-import javax.print.attribute.HashAttributeSet;
-
 public class BinaryTreeClient {
 
 	private static TreeNode root;
@@ -41,46 +39,146 @@ public class BinaryTreeClient {
 		}
 		System.out.println();
 
+		printSpiralLevelOrder(root);
+
 	}
-	
-//	public int hasPathSum(TreeNode a, int b)  {
-//		if(checkPathSum(a, b)) {
-//			return 1;
-//		}
-//		return 0;
-//	}
-	
+
+	public static ArrayList<ArrayList<Integer>>  printSpiralLevelOrder(TreeNode node) {
+		if (node == null) {
+			return null;
+		}
+		Stack<TreeNode> stackOne = new Stack<TreeNode>();
+		Stack<TreeNode> stackTwo = new Stack<TreeNode>();
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		
+		stackOne.add(node);
+
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		while (!stackOne.isEmpty() || !stackTwo.isEmpty()) {
+			
+			while (!stackOne.isEmpty()) {
+				TreeNode temp = stackOne.pop();
+				//System.out.print(temp.val + " ");
+			    row.add(temp.val);	
+				if (temp.left != null) {
+					stackTwo.push(temp.left);
+				}
+				if (temp.right != null) {
+					stackTwo.push(temp.right);
+				}
+			}
+			if (row.size() > 0) {
+				result.add(row);
+				row = new ArrayList<Integer>();
+			}
+			//System.out.println();
+			while (!stackTwo.isEmpty()) {
+				TreeNode temp = stackTwo.pop();
+				//System.out.print(temp.val + " ");
+				row.add(temp.val);
+				if (temp.right != null) {
+					stackOne.push(temp.right);
+				}
+				if (temp.left != null) {
+					stackOne.push(temp.left);
+				}
+			}
+			if (row.size() > 0) {
+				result.add(row);
+				row = new ArrayList<Integer>();
+			}
+			//System.out.println();
+		}
+		System.out.println("printing result for spiral order:");
+		for (ArrayList<Integer> R : result) {
+			System.out.println(R);
+		}
+		
+		return result;
+		
+	}
+
+	public static void spiralLevelOrder(TreeNode node) {
+		Stack<TreeNode> S1 = new Stack<TreeNode>();
+		Stack<TreeNode> S2 = new Stack<TreeNode>();
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+		S1.push(node);
+		spiralLevelOrderRecur(S1, S2, 1, result);
+		System.out.println("printing arraylist:");
+		for (ArrayList<Integer> R : result) {
+			System.out.println(R);
+		}
+	}
+
+	private static void spiralLevelOrderRecur(Stack<TreeNode> S1,
+			Stack<TreeNode> S2, int level, ArrayList<ArrayList<Integer>> result) {
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		while (!S1.isEmpty()) {
+			TreeNode node = S1.pop();
+
+			if (node != null) {
+				row.add(node.val);
+				// System.out.print(node.val +" ");
+			} else {
+				return;
+			}
+
+			int p = level % 2;
+			if (p == 1) {
+				S2.push(node.right);
+				S2.push(node.left);
+			} else {
+				S2.push(node.left);
+				S2.push(node.right);
+			}
+
+		}
+		// System.out.println();
+		result.add(row);
+		if (!S2.isEmpty()) {
+			spiralLevelOrderRecur(S2, S1, level + 1, result);
+		}
+	}
+
+	// public int hasPathSum(TreeNode a, int b) {
+	// if(checkPathSum(a, b)) {
+	// return 1;
+	// }
+	// return 0;
+	// }
+
 	boolean haspathSum(TreeNode node, int sum) {
-        if (node == null) {
-            return (sum == 0);
-        } else {
-            boolean ans = false;
- 
-            /* otherwise check both subtrees */
-            int subsum = sum - node.val;
-            if (subsum == 0 && node.left == null && node.right == null) {
-                return true;
-            }
-            if (node.left != null) {
-                ans = ans || haspathSum(node.left, subsum);
-            }
-            if (node.right != null) {
-                ans = ans || haspathSum(node.right, subsum);
-            }
-            return ans;
-        }
-    }
-	
+		if (node == null) {
+			return (sum == 0);
+		} else {
+			boolean ans = false;
+
+			/* otherwise check both subtrees */
+			int subsum = sum - node.val;
+			if (subsum == 0 && node.left == null && node.right == null) {
+				return true;
+			}
+			if (node.left != null) {
+				ans = ans || haspathSum(node.left, subsum);
+			}
+			if (node.right != null) {
+				ans = ans || haspathSum(node.right, subsum);
+			}
+			return ans;
+		}
+	}
+
 	public static boolean checkPathSum(TreeNode a, int b) {
 		if (a == null) {
-			return b == 0 ;
+			return b == 0;
 		} else {
 			b = b - a.val;
 			return (checkPathSum(a.left, b) || checkPathSum(a.right, b));
 		}
-		
+
 	}
-	
+
 	public static TreeNode sortedArrayToBST(final List<Integer> a) {
 		if (a == null) {
 			return null;
@@ -89,15 +187,15 @@ public class BinaryTreeClient {
 		int end = a.size() - 1;
 		return sortedArrayToBST(a, start, end);
 	}
-	
+
 	private static TreeNode sortedArrayToBST(List<Integer> A, int start, int end) {
 		if (end < start) {
 			return null;
 		}
-		int mid = start + (end - start)/2;
+		int mid = start + (end - start) / 2;
 		TreeNode node = new TreeNode(A.get(mid).intValue());
-		node.left = sortedArrayToBST(A, start, mid-1);
-		node.right = sortedArrayToBST(A, mid+1, end);
+		node.left = sortedArrayToBST(A, start, mid - 1);
+		node.right = sortedArrayToBST(A, mid + 1, end);
 		return node;
 	}
 

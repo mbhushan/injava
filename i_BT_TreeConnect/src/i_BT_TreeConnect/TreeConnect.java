b727520fab6c09cb8a189f739e8b9938f44dbdeb
manibhushan <manibhushan.cs@gmail.com>
2016-02-14 09:39:39 +0530

    connect tree

--- i_BT_TreeConnect/src/i_BT_TreeConnect/TreeConnect.java
+++ i_BT_TreeConnect/src/i_BT_TreeConnect/TreeConnect.java
@@ -11,6 +11,75 @@ class TreeLinkNode {
 
 public class TreeConnect {
 	
+	public TreeLinkNode getNextRight(TreeLinkNode p) {
+		TreeLinkNode temp = p.next;
+ 
+        /* Traverse nodes at p's level and find and return
+         the first node's first child */
+        while (temp != null) {
+            if (temp.left != null) {
+                return temp.left;
+            }
+            if (temp.right != null) {
+                return temp.right;
+            }
+            temp = temp.next;
+        }
+ 
+        // If all the nodes at p's level are leaf nodes then return NULL
+        return null;
+    }
+ 
+    /* Sets nextRight of all nodes of a tree with root as p */
+    public void connect(TreeLinkNode p) {
+    	TreeLinkNode temp = null;
+ 
+        if (p == null) {
+            return;
+        }
+ 
+        // Set nextRight for root
+        p.next = null;
+ 
+        // set nextRight of all levels one by one
+        while (p != null) {
+        	TreeLinkNode q = p;
+ 
+            /* Connect all childrem nodes of p and children nodes of all other nodes
+             at same level as p */
+            while (q != null) {
+                
+                // Set the nextRight pointer for p's left child
+                if (q.left != null) {
+                 
+                    // If q has right child, then right child is nextRight of
+                    // p and we also need to set nextRight of right child
+                    if (q.right != null) {
+                        q.left.next = q.right;
+                    } else {
+                        q.left.next = getNextRight(q);
+                    }
+                }
+ 
+                if (q.right != null) {
+                    q.right.next = getNextRight(q);
+                }
+ 
+                // Set nextRight for other nodes in pre order fashion
+                q = q.next;
+            }
+ 
+            // start from the first node of next level
+            if (p.left != null) {
+                p = p.left;
+            } else if (p.right != null) {
+                p = p.right;
+            } else {
+                p = getNextRight(p);
+            }
+        }
+    }
+	
 	public void connectRecur(TreeLinkNode p) {
 		 
         // Base case
@@ -52,7 +121,7 @@ public class TreeConnect {
     /* This function returns the leftmost child of nodes at the same level as p.
      This function is used to getNExt right of p's right child
      If right child of p is NULL then this can also be used for the left child */
-    public TreeLinkNode getNextRight(TreeLinkNode p) {
+    public TreeLinkNode getNextRight1(TreeLinkNode p) {
     	TreeLinkNode temp = p.next;
  
         /* Traverse nodes at p's level and find and return
@@ -71,7 +140,7 @@ public class TreeConnect {
         return null;
     }
 
-	public void connect(TreeLinkNode root) {
+	public void connect1(TreeLinkNode root) {
 
 		root.next = null;
 

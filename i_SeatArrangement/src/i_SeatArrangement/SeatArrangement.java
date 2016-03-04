8f1b2c1cc6c1bb535e617f31efe123702baf51a3
manibhushan <manibhushan.cs@gmail.com>
2016-03-04 15:36:27 +0530

    seat arrangement

--- i_SeatArrangement/src/i_SeatArrangement/SeatArrangement.java
+++ i_SeatArrangement/src/i_SeatArrangement/SeatArrangement.java
@@ -1,5 +1,30 @@
 package i_SeatArrangement;
 
+import java.util.ArrayList;
+
 public class SeatArrangement {
 
+	public static int seats(String A) {
+		
+		A = A.toLowerCase();
+		int N = A.length();
+		ArrayList<Integer> positions = new ArrayList<Integer>();
+		
+		for (int i=0; i<N; i++) {
+			if (A.charAt(i) == 'x') {
+				positions.add(i);
+			}
+		}
+		
+		int size = positions.size();
+		int mid = -1;
+		int emid1 = -1, emid2 = -1;
+		if (size%2 == 1) {
+			mid = (size - 1)/2;
+		} else {
+			
+		}
+				
+		
+	}
 }

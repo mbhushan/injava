9bfa7d96dc40468c27f6b35932a6032e7b1eec0e
manibhushan <manibhushan.cs@gmail.com>
2016-03-21 11:16:54 +0530

    automata implementation in java

--- i_Automata/src/i_Automata/Automata.java
+++ i_Automata/src/i_Automata/Automata.java
@@ -1,5 +1,27 @@
 package i_Automata;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Automata {
 
+	 public static int automata(ArrayList<Integer> a, ArrayList<Integer> b,
+	            ArrayList<Integer> c, int D, int n) {
+	        HashMap<Integer, Integer> map = new HashMap<>();
+	        int[][] table = new int[a.size()][n + 1];
+	        for (int i = 0; i < c.size(); i++) {
+	            map.put(c.get(i), 1);
+	        }
+	        for (int i = 0; i < a.size(); i++) {
+	            if (map.containsKey(i))
+	                table[i][0] = 1;
+	        }
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = 0; j < a.size(); j++) {
+	                table[j][i] = table[a.get(j)][i - 1] + table[b.get(j)][i - 1];
+	            }
+	        }
+
+	        return table[D][n];
+	    }
 }

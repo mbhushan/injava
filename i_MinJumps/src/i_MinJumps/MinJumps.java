aa22b75ed4c407430c5743434e3a9c2933d69b46
manibhushan <manibhushan.cs@gmail.com>
2016-03-21 11:17:11 +0530

    min jumps required

--- i_MinJumps/src/i_MinJumps/MinJumps.java
+++ i_MinJumps/src/i_MinJumps/MinJumps.java
@@ -1,5 +1,27 @@
 package i_MinJumps;
 
+import java.util.ArrayList;
+
 public class MinJumps {
 
+	public static void  minJumps(ArrayList<Integer> a, int b) {
+		int n = a.size();
+		int [] cost = new int[n+1];
+		int [] parent = new int[n+1];
+		cost[1] = a.get(0).intValue();
+		for (int i = 2; i <= n; i++) {
+			int mn = Integer.MAX_VALUE;
+			parent[i] = -1;
+			for (int j = i - 1; j >= (i - b); j++) {
+				if (a.get(j).intValue() != -1 && mn < cost[i]) {
+					mn = cost[i];
+					parent[i] = j;
+				}
+			}
+			if (a.get(i).intValue() != -1 && parent[i] != -1)
+				cost[i] = mn;
+			else
+				cost[i] = Integer.MAX_VALUE;
+		}
+	}
 }

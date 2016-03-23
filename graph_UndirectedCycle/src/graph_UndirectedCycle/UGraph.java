48c94af92d9c1a0ecc6f12776317a4cfbdbb85bb
manibhushan <manibhushan.cs@gmail.com>
2016-03-23 19:59:22 +0530

    added hasCycle util function

--- graph_UndirectedCycle/src/graph_UndirectedCycle/UGraph.java
+++ graph_UndirectedCycle/src/graph_UndirectedCycle/UGraph.java
@@ -1,10 +1,11 @@
 package graph_UndirectedCycle;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /*
  Detect cycle in an undirected graph
-Given an undirected graph, how to check if there is a cycle in the graph? 
+ Given an undirected graph, how to check if there is a cycle in the graph? 
  we can use DFS to detect cycle in an undirected graph in O(V+E) time. We do a DFS traversal of the given graph. For every visited vertex ‘v’, if there is an 
  adjacent ‘u’ such that u is already visited and u is not parent of v, then there is a cycle in graph. If we don’t find such an 
  adjacent for any vertex, we say that there is no cycle. The assumption of this approach is that there are no parallel edges between 
@@ -13,25 +14,25 @@ Given an undirected graph, how to check if there is a cycle in the graph?
 
 public class UGraph {
 	private int vertices;
-	private LinkedList<Integer> [] adjList;
-	
+	private LinkedList<Integer>[] adjList;
+
 	public UGraph(int v) {
 		vertices = v;
-		adjList = new LinkedList [v];
-		for (int i=0; i<v; i++) {
+		adjList = new LinkedList[v];
+		for (int i = 0; i < v; i++) {
 			adjList[i] = new LinkedList<Integer>();
 		}
 	}
-	
+
 	public void addEdge(int u, int v) {
 		adjList[u].add(v);
 		adjList[v].add(u);
 	}
-	
+
 	public boolean hasCycle() {
-		boolean [] visited = new boolean[vertices];
-		
-		for (int u=0; u < vertices; u++) {
+		boolean[] visited = new boolean[vertices];
+
+		for (int u = 0; u < vertices; u++) {
 			if (!visited[u]) {
 				if (hasCycleUtil(u, visited, -1)) {
 					return true;
@@ -40,5 +41,22 @@ public class UGraph {
 		}
 		return false;
 	}
-	
+
+	private boolean hasCycleUtil(int v, boolean[] visited, int parent) {
+		visited[v] = true;
+
+		Iterator<Integer> it = adjList[v].iterator();
+
+		while (it.hasNext()) {
+			int u = it.next();
+			if (!visited[u]) {
+				if (hasCycleUtil(u, visited, v))
+					return true;
+			} else if (u != parent) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }

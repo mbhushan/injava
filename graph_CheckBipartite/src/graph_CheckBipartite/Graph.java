afc105aadf13ceeaaf092423a029619cbb155715
manibhushan <manibhushan.cs@gmail.com>
2016-03-29 10:04:29 +0530

    checl bipartite

--- graph_CheckBipartite/src/graph_CheckBipartite/Graph.java
+++ graph_CheckBipartite/src/graph_CheckBipartite/Graph.java
@@ -1,6 +1,8 @@
 package graph_CheckBipartite;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
 
@@ -18,5 +20,30 @@ public class Graph {
 	
 	public void addEdge(int u, int v) {
 		adjList[u].add(v);
+	}
+	
+	public boolean isBipartite() {
+		int src = 0;
+		int [] colors = new int[vertices];
+		Arrays.fill(colors, -1);
+		
+		Queue<Integer> Q = new LinkedList<Integer>();
+		Q.add(src);
+		colors[src] = 1;
+		
+		while (!Q.isEmpty()) {
+			int u = Q.poll();
+			LinkedList<Integer> adj = adjList[u];
+			for (int v: adj) {
+				if (colors[v] == colors[u]) {
+					return false;
+				} else if (colors[v] == -1) {
+					colors[v] = 1 - colors[u];
+					Q.add(v);
+				}
+			}
+		}
+		
+		return true;
 	}
 }

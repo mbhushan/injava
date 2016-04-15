bd20f53cc2840409540bd89c7b453190e1fac574
manibhushan <manibhushan.cs@gmail.com>
2016-04-15 08:42:54 +0530

    president - lazy mode singleton pattern

---
+++ designpat_Singleton/src/designpat_Singleton/President.java
@@ -0,0 +1,20 @@
+package designpat_Singleton;
+
+/*
+ * http://www.programcreek.com/2011/07/java-design-pattern-singleton/
+ */
+
+//lazy mode
+public class President {
+
+	private static  President president;
+	
+	private President() {}
+	
+	public static President getPresident() {
+		if (president == null) {
+			president = new President();
+		}
+		return president;
+	}
+}

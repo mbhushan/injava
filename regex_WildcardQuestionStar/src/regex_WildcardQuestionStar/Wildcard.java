b182aed2009b9da65a2e29033920a78ae869d44d
manibhushan <manibhushan.cs@gmail.com>
2016-04-19 20:27:12 +0530

    wildcard evaluation

---
+++ regex_WildcardQuestionStar/src/regex_WildcardQuestionStar/Wildcard.java
@@ -0,0 +1,51 @@
+package regex_WildcardQuestionStar;
+
+/*
+ Implement wildcard pattern matching with support for '?' and '*'.
+ S = abcdefx
+ P = a?c*x
+ */
+
+public class Wildcard {
+	
+	public static void main(String [] args) {
+		String S = "abcdefx";
+		String P = "a?c*x";
+		Wildcard wc = new Wildcard();
+		
+		System.out.println("is match: " + wc.isMatch(S, P));
+	}
+
+	
+	public boolean isMatch(String S, String P) {
+		int iStr = 0;
+		int jPat = 0;
+		int startIndex = -1;
+		int iIndex = -1;
+		int slen = S.length();
+		int plen = P.length();
+		
+		while (iStr < slen) {
+			if ((jPat < plen) && (P.charAt(jPat) == S.charAt(iStr) || P.charAt(jPat) == '?')) {
+				++iStr;
+				++jPat;
+			} else if ((jPat < plen) && (P.charAt(jPat) == '*')) {
+				startIndex = jPat;
+				iIndex = iStr;
+				++jPat;
+			} else if ((jPat < plen) && (startIndex != -1)) {
+				jPat = startIndex + 1;
+				iStr = iIndex + 1;
+				++iIndex;
+			} else {
+				return false;
+			}
+		}
+		while (jPat < P.length() && P.charAt(jPat) == '*') {
+			++jPat;
+		}
+		
+		return jPat == plen;
+		
+	}
+}

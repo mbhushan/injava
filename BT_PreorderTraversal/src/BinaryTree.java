e6caae8b42dee0aa884673096da30d1475d7937b
manibhushan <manibhushan.cs@gmail.com>
2016-04-20 12:04:53 +0530

    binary tree class

---
+++ BT_PreorderTraversal/src/BinaryTree.java
@@ -0,0 +1,61 @@
+
+
+class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int value) {
+		this.value = value;
+		left = null;
+		right = null;
+	}
+}
+
+public class BinaryTree {
+	private TreeNode root ;
+	
+	BinaryTree() {
+		root = null;
+	}
+	
+	
+	public void inorder() {
+		inorder(root);
+	}
+	
+	private void inorder(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+		if (node.left != null) {
+			inorder(node.left);
+		}
+		System.out.print(node.value + " ");
+		if (node.right != null) {
+			inorder(node.right);
+		}
+	}
+	
+	public void buildBinaryTree(int [] A) {
+		if (null == A) {
+			return ;
+		}
+		int len = A.length;
+		for (int i=0; i<len; i++) {
+			root = insertIntoBT(root, A[i]);
+		}
+	}
+	
+	private TreeNode insertIntoBT(TreeNode node, int key) {
+		if (node == null) {
+			return new TreeNode(key);
+		}
+		if (key <= node.value) {
+			node.left = insertIntoBT(node.left, key);
+		} else {
+			node.right = insertIntoBT(node.right, key);
+		}
+		return node;
+	}
+}

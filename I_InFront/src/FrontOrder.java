0d869a34659344ba0be293c874d20009b95706a5
manibhushan <manibhushan.cs@gmail.com>
2016-04-30 08:43:38 +0530

    height ordering - minor fix

--- I_InFront/src/FrontOrder.java
+++ I_InFront/src/FrontOrder.java
@@ -6,18 +6,22 @@ import java.util.Comparator;
 
 /*
 PROBLEM:
-You are given two array, first array contain integer which represent heights of persons and second array contain how many persons in front of him are standing who are greater than him in term of height and forming a queue. Ex 
+You are given two array, first array contain integer which represent heights of persons and second 
+array contain how many persons in front of him are standing who are greater than him in term of height and forming a queue. Ex 
 A: 3 2 1 
 B: 0 1 1 
-It means in front of person of height 3 there is no person standing, person of height 2 there is one person in front of him who has greater height then he, similar to person of height 1. Your task to arrange them 
-Ouput should be. 
+It means in front of person of height 3 there is no person standing, person of height 2 there is 
+one person in front of him who has greater height then he, similar to person of height 1. 
+Your task to arrange them Ouput should be. 
 3 1 2 
 Here - 3 is at front, 1 has 3 in front ,2 has 1 and 3 in front.
 
 This can be solved using rope data structure. 
-It's like a binary tree except that each node maintains the number of nodes in the left subtree+1 for itself. Whenever a new number is inserted, if the value is smaller than the node's number it goes to the left otherwise right. When it goes to the right, the value of the node is decreased by the value of the branch node. 
+It's like a binary tree except that each node maintains the number of nodes in the left subtree+1 for itself.
+Whenever a new number is inserted, if the value is smaller than the node's number it goes to the left otherwise right.
+When it goes to the right, the value of the node is decreased by the value of the branch node. 
 Ex Nodes:	6	5	4	3	2	1 
-values:	0	0	0	2	2	4 
+  values:	0	0	0	2	2	4 
 1. Make 6 as the root of the tree, its value = 1; 
 2. Insert 5. Value of 5(0) < value of 6(1) so it goes to the left. New value of 6 = 2, value of 5=1; 
 3. Insert 4, value of 4 < value of 6 so goes to the left; again goes to the left of 5. New values of 4 = 1, value of 5 = 2, value of 6 = 3 

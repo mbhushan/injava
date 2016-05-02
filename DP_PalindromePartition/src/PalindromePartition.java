4e4fd53cd54882278ee40df16956bb0dfd33f57f
manibhushan <manibhushan.cs@gmail.com>
2016-05-02 09:17:13 +0530

    palindromic partition

--- DP_PalindromePartition/src/PalindromePartition.java
+++ DP_PalindromePartition/src/PalindromePartition.java
@@ -9,8 +9,10 @@ public class PalindromePartition {
 	public static void main(String [] args) {
 		PalindromePartition PP = new PalindromePartition();
 		String string = "abcbm";
+		//string = "abcde";
 		int minCut = PP.palindromePartitionMinCut(string);
 		System.out.println("min cut: " + minCut);
+		System.out.println("min cut str: " + PP.minCut(string));
 	}
 
 	public int palindromePartitionMinCut(String string) {
@@ -25,23 +27,33 @@ public class PalindromePartition {
 		}
 		
 		int [][] T = new int[len][len];
+		boolean [][] P = new boolean[len][len];
 		// zero splits required for individual character strings.
 		for (int i=0; i<len; i++) {
 			T[i][i] = 0;
+			P[i][i] = true;
 		}
 		
-		for (int l=1; l<=len; l++) {
+		for (int l=2; l<=len; l++) {
 			for (int i=0; i<len - (l-1);i++) {
 				int j = i + (l-1);
-				if (isPalindrome(string, i, j)) {
+				
+				// If L is 2, then we just need to compare two characters. Else
+	            // need to check two corner characters and value of P[i+1][j-1]
+	            if (l == 2)
+	                P[i][j] = (string.charAt(i) == string.charAt(j));
+	            else
+	                P[i][j] = (string.charAt(i) == string.charAt(j)) && P[i+1][j-1];
+	            
+				//if (isPalindrome(string, i, j)) {
+				if (P[i][j]) {
 					T[i][j] = 0;
 					continue;
 				}
-				int min = Integer.MAX_VALUE;
-				for (int k=i+1; k<=j; k++) {
-					min = Math.min(min, Math.min(T[i][k-1], T[k][j]));
+				T[i][j] = Integer.MAX_VALUE;
+				for (int k=i; k<j; k++) {
+					T[i][j] = Math.min(T[i][j], T[i][k] + T[k+1][j] + 1);
 				}
-				T[i][j] = 1 + min;
 			}
 		}
 		
@@ -50,6 +62,26 @@ public class PalindromePartition {
 	}
 	
 	
+	public int minCut(String str){
+        if (str.length() == 0) {
+            return 0;
+        }
+
+        int[] cut = new int[str.length()];
+        boolean isPal[][] = new boolean[str.length()][str.length()];
+        for (int i = 1; i < str.length(); i++) {
+            int min = i;
+            for (int j = 0; j <= i; j++) {
+                if (str.charAt(i) == str.charAt(j) && (i <= j + 1 || isPal[i - 1][j + 1])) {
+                    isPal[i][j] = true;
+                    min = Math.min(min, j == 0 ? 0 : 1 + cut[j - 1]);
+                }
+            }
+            cut[i] = min;
+        }
+        return cut[str.length() - 1];
+    }
+
 	
 	 private  boolean isPalindrome(String str, int r, int t) {
 	        while(r < t) {

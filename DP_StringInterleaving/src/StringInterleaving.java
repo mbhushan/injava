48fde30a2b5a10b7155d3a73a842cae9639c776a
manibhushan <manibhushan.cs@gmail.com>
2016-05-05 07:03:32 +0530

    string interleaving check - added DP method

--- DP_StringInterleaving/src/StringInterleaving.java
+++ DP_StringInterleaving/src/StringInterleaving.java
@@ -2,6 +2,38 @@
 public class StringInterleaving {
 	
 	
+	public boolean isInterleavedDP(String S1, String S2, String S3) {
+		char [] A = S1.toCharArray();
+		char [] B = S2.toCharArray();
+		char [] C = S3.toCharArray();
+		
+		boolean [][] T = new boolean[A.length+1][B.length+1];
+		
+		if (A.length + B.length != C.length) {
+			return false;
+		}
+		
+		for (int i=0; i<=A.length; i++) {
+			for (int j=0; j<=B.length; j++) {
+				int k = i + j -1;
+				if (i == 0 && j == 0) {
+					T[i][j] = true;
+				} else if (i == 0) {
+					if (B[j-1] == C[k]) {
+						T[i][j] = T[i][j-1];
+					}
+				} else if (j ==0) {
+					if (A[i-1] == C[k]) {
+						T[i][j] = T[i-1][j];
+					}
+				} else {
+					T[i][j] = (A[i-1] == C[k] ? T[i-1][j]: false) || (B[j-1] == C[k] ? T[i][j-1] : false);
+				}
+			}
+		}
+		return T[A.length][B.length];
+		
+	}
 
 	public static void main(String[] args) {
 		StringInterleaving SI = new StringInterleaving();
@@ -10,9 +42,11 @@ public class StringInterleaving {
 		String S3 = "aaxaby";
 		boolean ans = SI.isInterleaved(S1.toCharArray(), S2.toCharArray(), S3.toCharArray(), 0, 0, 0);
 		System.out.println("is interleaved: " + ans);
+		System.out.println("is interleaved DP: " + SI.isInterleavedDP(S1, S2, S3));
 		S3 = "abaaxy";
 		ans = SI.isInterleaved(S1.toCharArray(), S2.toCharArray(), S3.toCharArray(), 0, 0, 0);
 		System.out.println("is interleaved: " + ans);
+		System.out.println("is interleaved DP: " + SI.isInterleavedDP(S1, S2, S3));
 	}
 	
 	public boolean isInterleaved(char [] S1, char [] S2, char [] S3, int p1, int p2, int p3) {

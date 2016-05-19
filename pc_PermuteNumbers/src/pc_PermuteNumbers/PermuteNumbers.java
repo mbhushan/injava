b12b99bd7fefd81937cd2e5a6e152e951a66d369
manibhushan <manibhushan.cs@gmail.com>
2016-05-19 12:27:08 +0530

    permute numbers

--- pc_PermuteNumbers/src/pc_PermuteNumbers/PermuteNumbers.java
+++ pc_PermuteNumbers/src/pc_PermuteNumbers/PermuteNumbers.java
@@ -4,6 +4,34 @@ import java.util.ArrayList;
 
 public class PermuteNumbers {
 	
+	public ArrayList<ArrayList<Integer>> permute(int [] A) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		permute(A, 0, result);
+		return result;
+		
+	}
+	
+	private void permute(int [] A, int start, ArrayList<ArrayList<Integer>> result) {
+		if (start >= A.length) {
+			ArrayList<Integer> values = new ArrayList<Integer>(A.length);
+			for (Integer x: A) {
+				values.add(x);
+			}
+			result.add(values);
+		}
+			for (int i=start; i<A.length; i++) {
+				swap(A, start, i);
+				permute(A, start+1, result);
+				swap(A, start, i);
+			}
+	}
+	
+	private void swap(int [] A, int i, int j) {
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+	}
+	
 	public ArrayList<ArrayList<Integer>> permuteNumbers(int [] A) {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		//start with empty list
@@ -21,8 +49,6 @@ public class PermuteNumbers {
 					ArrayList<Integer> temp = new ArrayList<Integer>(l);
 					current.add(temp);
 	 
-					//System.out.println(temp);
-	 
 					// - remove A[i] add
 					l.remove(j);
 				}
@@ -38,7 +64,8 @@ public class PermuteNumbers {
 		PermuteNumbers PN = new PermuteNumbers();
 		
 		int [] A = {1, 2, 3};
-		ArrayList<ArrayList<Integer>> result = PN.permuteNumbers(A);
+//		ArrayList<ArrayList<Integer>> result = PN.permuteNumbers(A);
+		ArrayList<ArrayList<Integer>> result = PN.permute(A);
 		for (ArrayList<Integer> R: result) {
 			for (Integer x: R) {
 				System.out.print(x + " ");

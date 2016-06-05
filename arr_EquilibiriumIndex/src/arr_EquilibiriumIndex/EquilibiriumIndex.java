c8e402a5f4bdcaae3920446413f4f7889a245ea3
manibhushan <manibhushan.cs@gmail.com>
2016-06-05 09:15:26 +0530

    find equilibirium array index

---
+++ arr_EquilibiriumIndex/src/arr_EquilibiriumIndex/EquilibiriumIndex.java
@@ -0,0 +1,56 @@
+package arr_EquilibiriumIndex;
+
+/*
+Equilibrium index of an array
+Equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes. 
+For example, in an array A:
+
+A[0] = -7, A[1] = 1, A[2] = 5, A[3] = 2, A[4] = -4, A[5] = 3, A[6]=0
+
+3 is an equilibrium index, because:
+A[0] + A[1] + A[2] = A[4] + A[5] + A[6]
+
+6 is also an equilibrium index, because sum of zero elements is zero, i.e., A[0] + A[1] + A[2] + A[3] + A[4] + A[5]=0
+
+7 is not an equilibrium index, because it is not a valid index of array A.
+
+Write a function int equilibrium(int[] arr, int n); that given a sequence arr[] of size n, returns an equilibrium index (if any) or -1 
+if no equilibrium indexes exist.
+*/
+
+public class EquilibiriumIndex {
+
+	public static void main(String[] args) {
+		EquilibiriumIndex EI = new EquilibiriumIndex();
+		
+		int [] A = {-7, 1, 5, 2, -4, 3, 0};
+		
+		int index = EI.equilibirium(A);
+		System.out.println("index: " + index);
+		
+		
+	}
+	
+	private int equilibirium(int [] A) {
+		if (A == null || A.length < 1) {
+			return -1;
+		}
+		
+		int leftSum = 0;
+		int sum = 0;
+		
+		for (int i=0; i<A.length; i++) {
+			sum += A[i];
+		}
+		
+		for (int i=0; i<A.length; i++) {
+			sum = sum - A[i];
+			if (leftSum == sum) {
+				return i;
+			}
+			
+			leftSum += A[i];
+		}
+		return -1;
+	}
+}

0079ba1ee077a2a6bf41d4f7d3e3a268b3d75670
manibhushan <manibhushan.cs@gmail.com>
2016-06-06 09:11:01 +0530

    undirected graph all paths one node

---
+++ fx_UnidrectedGraphAllPathOneNode/src/fx_UnidrectedGraphAllPathOneNode/GraphOperation.java
@@ -0,0 +1,9 @@
+package fx_UnidrectedGraphAllPathOneNode;
+
+/*
+Round 1: Given an undirected graph and a node, modify the graph into a directed graph such that, any path leads to one particular node.
+*/
+
+public class GraphOperation {
+
+}

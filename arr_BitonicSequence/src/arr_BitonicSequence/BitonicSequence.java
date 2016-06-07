c7921cb83c025ca8768f949fb79d2ce05446614b
manibhushan <manibhushan.cs@gmail.com>
2016-06-07 06:51:07 +0530

    bitonic sequence

---
+++ arr_BitonicSequence/src/arr_BitonicSequence/BitonicSequence.java
@@ -0,0 +1,12 @@
+package arr_BitonicSequence;
+
+/*
+Maximum Length Bitonic Subarray
+Given an array A[0 … n-1] containing n positive integers, a subarray A[i … j] is bitonic 
+if there is a k with i <= k <= j such that A[i] <= A[i + 1] ... <= A[k] >= A[k + 1] >= .. A[j – 1] > = A[j]. 
+Write a function that takes an array as argument and returns the length of the maximum length bitonic subarray.
+Expected time complexity of the solution is O(n)
+ */
+public class BitonicSequence {
+
+}

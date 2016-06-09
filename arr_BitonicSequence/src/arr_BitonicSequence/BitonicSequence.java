72301f2dad3968cb9f8d20551724dd1098014607
manibhushan <manibhushan.cs@gmail.com>
2016-06-09 10:08:11 +0530

    bitonic sequence

--- arr_BitonicSequence/src/arr_BitonicSequence/BitonicSequence.java
+++ arr_BitonicSequence/src/arr_BitonicSequence/BitonicSequence.java
@@ -9,4 +9,47 @@ Expected time complexity of the solution is O(n)
  */
 public class BitonicSequence {
 
+	public static void main(String[] args) {
+		BitonicSequence bs = new BitonicSequence();
+		int [] A = {12, 4, 78, 90, 45, 23};
+		
+		System.out.println("max bitonic sequence: "  + bs.maxBitonicSeq(A));
+	}
+	
+	public int maxBitonicSeq(int [] A) {
+		if (A == null || A.length < 1) {
+			return 0;
+		}
+		
+		int result = 0;
+		int [] inc = new int[A.length];
+		int [] dec = new int[A.length];
+		
+		inc[0] = 1;
+		for (int i=1; i<A.length; i++) {
+			if (A[i] > A[i-1]) {
+				inc[i] = inc[i-1] + 1;
+			} else {
+				inc[i] = 1;
+			}
+		}
+		
+		dec[A.length - 1] = 1;
+		for (int i=A.length-2; i>=0 ;i--) {
+			if (A[i] > A[i+1]) {
+				dec[i] = dec[i+1] + 1;
+			} else {
+				dec[i] = 1;
+			}
+		}
+		
+		for (int i=0; i<A.length; i++) {
+			int tmp = inc[i] + dec[i] - 1;
+			if (tmp > result) {
+				result = tmp;
+			}
+		}
+		
+		return result;
+	}
 }

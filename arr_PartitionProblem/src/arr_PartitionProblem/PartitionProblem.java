70800a3f29bd30837c7d578cdb30cf92e9e2770b
manibhushan <manibhushan.cs@gmail.com>
2016-06-15 09:00:38 +0530

    array equal parition with DP

--- arr_PartitionProblem/src/arr_PartitionProblem/PartitionProblem.java
+++ arr_PartitionProblem/src/arr_PartitionProblem/PartitionProblem.java
@@ -7,7 +7,42 @@ public class PartitionProblem {
 		
 		int [] A = {3, 1, 5, 9, 12};
 		System.out.println("equal partition possible: " + pp.partition(A));
+		System.out.println("equal partition possible fast: " + pp.partitionDP(A));
 		
+	}
+	
+	public boolean partitionDP(int [] A) {
+		if (A == null || A.length <= 1) {
+			return false;
+		}
+		int sum = 0;
+		for (int i=0; i<A.length; i++) {
+			sum += A[i];
+		}
+		if (sum%2 == 1) {
+			return false;
+		}
+		sum = sum/2;
+		boolean [][] dp = new boolean[sum+1][A.length+1];
+		
+		for (int i=0; i<=A.length; i++) {
+			dp[0][i] = true;
+		}
+		
+		for (int i=1; i<=sum; i++) {
+			dp[i][0] = false; 
+		}
+		
+		for (int i=1; i<=sum; i++) {
+			for (int j=1; j<=A.length; j++) {
+				dp[i][j] = dp[i][j-1];
+				if (i >= A[j-1]) {
+					dp[i][j] = dp[i][j] || dp[i - A[j-1]][j-1];
+				}
+			}
+		}
+		
+		return dp[sum][A.length];
 	}
 	
 	public boolean partition(int [] A) {

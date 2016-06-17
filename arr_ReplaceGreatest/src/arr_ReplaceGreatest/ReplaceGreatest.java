38f0c017cc33b412df80942cafaa4b7e52bfac56
manibhushan <manibhushan.cs@gmail.com>
2016-06-17 08:12:43 +0530

    replace greatest in an array

---
+++ arr_ReplaceGreatest/src/arr_ReplaceGreatest/ReplaceGreatest.java
@@ -0,0 +1,22 @@
+package arr_ReplaceGreatest;
+
+/*
+Replace every element with the greatest element on right side
+Given an array of integers, replace every element with the next greatest element (greatest element on the right side) in the array. 
+Since there is no element next to the last element, replace it with -1. For example, if the array is {16, 17, 4, 3, 5, 2}, 
+then it should be modified to {17, 5, 5, 5, 2, -1}.
+*/
+
+public class ReplaceGreatest {
+
+	public static void main(String[] args) {
+		ReplaceGreatest rg = new ReplaceGreatest();
+		
+	}
+	
+	public void replaceGreatest(int [] A) {
+		if (A == null || A.length < 1) {
+			return ;
+		}
+	}
+}

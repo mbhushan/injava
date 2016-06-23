d0722e81b310e2dbb94ce259cfc08ac59561083c
manibhushan <manibhushan.cs@gmail.com>
2016-06-23 06:56:47 +0530

    egg dropping

---
+++ in_EggDropping/src/in_EggDropping/EggDropping.java
@@ -0,0 +1,34 @@
+package in_EggDropping;
+
+public class EggDropping {
+
+	public static void main(String[] args) {
+		EggDropping ed = new EggDropping();
+		int eggs = 2;
+		int floors = 6;
+		System.out.println("num of attempts: " + ed.calculate(eggs, floors));
+	}
+
+	public int calculate(int eggs, int floors) {
+
+		int T[][] = new int[eggs + 1][floors + 1];
+		int c = 0;
+		for (int i = 0; i <= floors; i++) {
+			T[1][i] = i;
+		}
+
+		for (int e = 2; e <= eggs; e++) {
+			for (int f = 1; f <= floors; f++) {
+				T[e][f] = Integer.MAX_VALUE;
+				for (int k = 1; k <= f; k++) {
+					c = 1 + Math.max(T[e - 1][k - 1], T[e][f - k]);
+					if (c < T[e][f]) {
+						T[e][f] = c;
+					}
+				}
+			}
+		}
+		return T[eggs][floors];
+	}
+
+}

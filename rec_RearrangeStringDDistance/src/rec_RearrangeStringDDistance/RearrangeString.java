5d08cc6845dc362e1cefad40f3142b9881d71420
manibhushan <manibhushan.cs@gmail.com>
2016-06-30 08:26:13 +0530

    rearrange strings so that same chars are d dist away

---
+++ rec_RearrangeStringDDistance/src/rec_RearrangeStringDDistance/RearrangeString.java
@@ -0,0 +1,28 @@
+package rec_RearrangeStringDDistance;
+
+/*
+Rearrange a string so that all same characters become d distance away
+Given a string and a positive integer d. Some characters may be repeated in the given string. Rearrange characters of the given string such that the same characters become d distance away from each other. Note that there can be many possible rearrangements, the output should be one of the possible rearrangements. If no such arrangement is possible, that should also be reported.
+Expected time complexity is O(n) where n is length of input string.
+
+Examples:
+Input:  "abb", d = 2
+Output: "bab"
+
+Input:  "aacbbc", d = 3
+Output: "abcabc"
+
+Input: "geeksforgeeks", d = 3
+Output: egkegkesfesor
+
+Input:  "aaa",  d = 2
+Output: Cannot be rearranged
+*/
+
+public class RearrangeString {
+
+	public static void main(String[] args) {
+		RearrangeString rs = new RearrangeString();
+		
+	}
+}

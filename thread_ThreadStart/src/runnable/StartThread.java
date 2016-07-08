79883ddde97155bbc972fed4d3a83e9428cebc27
manibhushan <manibhushan.cs@gmail.com>
2016-07-08 08:20:23 +0530

    thread start by implementing runnable interface

---
+++ thread_ThreadStart/src/runnable/StartThread.java
@@ -0,0 +1,29 @@
+package runnable;
+
+public class StartThread {
+
+	public static void main(String[] args) {
+		
+		Thread t1 = new Thread(new Runner());
+		Thread t2 = new Thread(new Runner());
+		
+		t1.start();
+		t2.start();
+	}
+}
+
+class Runner implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i=0; i<5; i++) {
+			System.out.println("counting: " + i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}

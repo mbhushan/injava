54ce9f9b2d71b87225fbedc186e16f7378896976
manibhushan <manibhushan.cs@gmail.com>
2016-07-08 08:25:53 +0530

    thread synch code block main class

---
+++ thread_SynchronizationCodeBlock/src/thread_SynchronizationCodeBlock/SynchronizationBlock.java
@@ -0,0 +1,9 @@
+package thread_SynchronizationCodeBlock;
+
+public class SynchronizationBlock {
+
+	public static void main(String[] args) {
+		Worker worker = new Worker();
+		worker.init();
+	}
+}

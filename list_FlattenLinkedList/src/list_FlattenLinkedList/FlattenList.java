1a4555b65f1c55190804115733265f64b4a38961
manibhushan <manibhushan.cs@gmail.com>
2016-07-09 08:12:23 +0530

    flatten linked list - initial commit

---
+++ list_FlattenLinkedList/src/list_FlattenLinkedList/FlattenList.java
@@ -0,0 +1,16 @@
+package list_FlattenLinkedList;
+
+public class FlattenList {
+
+}
+
+class ListNode {
+	
+	int value;
+	ListNode right;
+	ListNode down;
+	
+	public ListNode(int value) {
+		this.value = value;
+	}
+}

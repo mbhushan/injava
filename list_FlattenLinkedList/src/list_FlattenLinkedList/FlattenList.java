6f8a89d79f2c07dabd3ad9aa08d11bbe65e1b209
manibhushan <manibhushan.cs@gmail.com>
2016-07-11 09:03:13 +0530

    flatten list - first commit

--- list_FlattenLinkedList/src/list_FlattenLinkedList/FlattenList.java
+++ list_FlattenLinkedList/src/list_FlattenLinkedList/FlattenList.java
@@ -2,6 +2,22 @@ package list_FlattenLinkedList;
 
 public class FlattenList {
 
+	public static void main(String[] args) {
+		
+	}
+	
+	public void flattenList(ListNode node) {
+		
+		if (node == null) {
+			return;
+		}
+		
+		if (node.down != null) {
+			node.down.right = node.right;
+			node.right = node.down;
+			node.down = null;
+		}
+	}
 }
 
 class ListNode {

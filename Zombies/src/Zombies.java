075ae9d8f606d0beeed9baea2f50f357a645c99c
manibhushan <manibhushan.cs@gmail.com>
2016-08-22 20:18:20 -0700

    find connected zombies

---
+++ Zombies/src/Zombies.java
@@ -0,0 +1,47 @@
+import javax.swing.text.ZoneView;
+
+
+public class Zombies {
+
+	static int zombieCluster(String[] zombies) {
+		if (zombies == null || zombies.length < 1) {
+			return 0;
+		}
+		
+		int r = zombies.length;
+		int c = zombies[0].length();
+		
+		int [][] M = new int[r][c];
+		for (int i=0; i<r; i++) {
+			char [] in = zombies[i].toCharArray();
+			for (int j=0; j<c; j++) {
+				M[i][j] = Character.getNumericValue(in[j]);
+			}
+		}
+		
+		boolean [] visited = new boolean[r];
+		int cluster = 0;
+		for (int i=0; i<r; i++) {
+			if (!visited[i]) {
+				dfs(i, M, visited);
+				++cluster;
+			}
+		}
+		return cluster;
+
+    }
+	
+	static void dfs(int u, int [][] M, boolean [] visited) {
+		visited[u] = true;
+		
+		for (int i=0; i<M[u].length; i++) {
+			
+			int val = M[u][i];
+			if (val > 0) {
+				if (!visited[i]) {
+					dfs(i, M, visited);
+				}
+			}
+		}
+	}
+}
